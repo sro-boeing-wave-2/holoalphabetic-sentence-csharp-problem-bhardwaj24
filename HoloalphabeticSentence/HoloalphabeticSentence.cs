411cf83f580503f47e2dac575ecadc3c0e4a8c61
Abhishek Bhardwaj <abhishek@mail.com>
2018-07-19 11:08:22 +0530

    First Commit

--- HoloalphabeticSentence/HoloalphabeticSentence.cs
+++ HoloalphabeticSentence/HoloalphabeticSentence.cs
@@ -8,7 +8,29 @@ namespace HoloalphabeticSentence
         // Write the logic to determine whether the input string is a HoloalphabeticSentence or not
         public static bool IsHoloalphabeticSentence(string input, string alphabets = "abcdefghijklmnopqrstuvwxyz")
         {
-            return false;
+            int i,j=0,count=0;
+            string lowercase = input.ToLower();
+            for (i= 0;i<alphabets.Length;i++)
+            {
+                for(j=0;j<lowercase.Length;j++)
+                {
+                    if(alphabets[i]==lowercase[j])
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            if(count==26)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
         }
     }
 }
